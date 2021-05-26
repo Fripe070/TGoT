5655cc2af80c9631f817aaf2162dedbde58244c8
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2021-05-26 18:50:09 -0500

    🔉 We now have sound! When colliding with a traffic cone a sound will play. Additionally, I have refactored/cleaned up the code (in favor of human-readability) and added more comments.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,12 @@
 // ReSharper disable RedundantUsingDirective
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,11 +14,14 @@ public class PlayerMovement : MonoBehaviour
     public Camera mainCamera;
     public Camera hoodCamera;
     public KeyCode cameraSwitchKey = KeyCode.G;
+    
+    // Audio
+    public AudioSource coneSound;
 
     // Start is called before the first frame update
     void Start()
     {
-        print("Start has been called.");
+        coneSound = GetComponent<AudioSource>();
     }
 
     // Private variables for the driving/turning speeds.
@@ -23,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
     private float horizontalInput;
     private float forwardInput;
     // Update is called once per frame
+    [SuppressMessage("ReSharper", "Unity.InefficientPropertyAccess")]
     void Update()
     {
         // Set our axis
@@ -33,24 +41,23 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
         
-        if(Input.GetKeyDown(cameraSwitchKey))  // If the user is pressing the camera switcher key
-        { mainCamera.enabled = !mainCamera.enabled; hoodCamera.enabled = !hoodCamera.enabled; }
+        // If the user is pressing the camera switcher key
+        if(Input.GetKeyDown(cameraSwitchKey)) { mainCamera.enabled = !mainCamera.enabled; hoodCamera.enabled = !hoodCamera.enabled; }
 
-        if (transform.position.x < -8f)
-        {
+        // Invisible boundaries for the X axis (left/right)
+        if (transform.position.x < -8f) { transform.position = new Vector3(-7.9f, transform.position.y, transform.position.z); }
+        else if (transform.position.x > 28f) { transform.position = new Vector3(27.9f, transform.position.y, transform.position.z); }
 
-            var transform1 = transform;
-            var position = transform1.position;
-            position = new Vector3(-7.9f, position.y, position.z);
-            transform1.position = position;
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
+        // Check the car's rotation, if it is upside down (y=-1) then flip the car back up (y=1):
+        if (transform.up.y == -1f) { transform.rotation = Quaternion.Euler(transform.up.x,1,transform.up.z); }
+
+    }
+    // Event function for when we collide with an object:
+    void OnCollisionEnter(Collision collision) {
+        // If we collide with a Traffic Cone [Obstacle], play a traffic cone sound.
+        if (collision.gameObject.CompareTag("TrafficConeObstacle")) {
+            coneSound.Play();
         }
-        else if (transform.position.x > 28f)
-        {
-            var transform1 = transform;
-            var position = transform1.position;
-            position = new Vector3(27.9f, position.y, position.z);
-            transform1.position = position;
-        }
-
     }
 }
